88bfc75fb59a63c66b46cfcdff6d4619f0047a3d
Christian Brevik <christian.brevik@gmail.com>
2026-06-05 08:40:03 +0200

    Add launcher for games

--- main.go
+++ main.go
@@ -2,27 +2,16 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 func main() {
-	args := os.Args[1:]
-
-	activeGame := ""
-	if len(args) > 0 {
-		gameToRun := args[0]
-		if len(gameToRun) > 0 {
-			activeGame = gameToRun
-		}
-	}
-
-	game := &Game{activeGame: activeGame}
+	game := &Game{currentScene: NewLauncherScene()}
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	ebiten.SetWindowSize(426*4, 240*4)
 	ebiten.SetWindowTitle("GOTY2026")
-	if err := ebiten.RunGame(game); err != nil {
+	if err := ebiten.RunGameWithOptions(game, &ebiten.RunGameOptions{InitUnfocused: true}); err != nil {
 		log.Fatal(err)
 	}
 }
